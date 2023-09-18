66852fe13602842d9ec28728d9e12605799720e2
thestackdev <shanmukeshwar03@gmail.com>
2023-09-18 10:20:57 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     serverActions: true,
   },
